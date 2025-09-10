db0e9250fab681df74b8633ccd2f23a5e92fa28b
Kirill Abrosimov <kirill.abrosimov@openinnovation.ai>
2025-09-10 16:34:23 +0400

    install fzf + zen. add forgotten aliases for git

--- roles/devbox/files/.config/fish/conf.d/aliases.fish
+++ roles/devbox/files/.config/fish/conf.d/aliases.fish
@@ -1,5 +1,16 @@
 abbr -a co git checkout
-abbr -a cob git checkout -b
 abbr -a st git status
-abbr -a stash git stash # TODO: In the video on YT there was a nice hack for git stash.
+abbr -a gd git diff
+abbr -a am git commit --amend
+
+abbr -a cob git checkout -b
+abbr -a add git add
+abbr -a gdc git diff --cached
+
 abbr -a pull git pull origin 
+abbr -a push git push origin
+
+abbr -a stash git stash # TODO: In the video on YT there was a nice hack for git stash.
+
+abbr -a --set-cursor gg "git grep -n '%'"
+abbr -a --set-cursor ci "git commit -m '%'"

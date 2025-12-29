e4271305541acd97fd580042d19043332799c95d
Kirill Abrosimov <kirill.abrosimov@openinnovation.ai>
2025-12-29 13:48:52 +0400

    space is a half page down now, . is leader key for nvim

--- roles/devbox/files/.config/nvim/init.lua
+++ roles/devbox/files/.config/nvim/init.lua
@@ -1,8 +1,11 @@
--- Set <space> as the leader key
+-- Set dot (.) as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = '.'
+vim.g.maplocalleader = '.'
+
+-- Map space to half-page down in normal mode
+vim.keymap.set('n', '<Space>', '<C-d>', { desc = 'Half page down' })
 
 vim.opt.number = true
 -- Enable break indent
